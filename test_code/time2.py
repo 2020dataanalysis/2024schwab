471dcafe8cbc0a1e75932a4dcd2042bfb8a7f78d
2020dataanalysis <2019supersam@gmail.com>
2024-04-29 11:29:57 -0700

    utc

--- test_code/time2.py
+++ test_code/time2.py
@@ -1,17 +1,4 @@
 from datetime import datetime, timedelta, timezone
 
-minutes = 30
-# start_time = datetime.now() - timedelta(minutes=minutes)
-
-
-pt_offset = -7  # Pacific Daylight Time (PDT) offset from UTC
-
-# Get current UTC time
-# current_utc_time = datetime.utcnow()
-now = datetime.now()
 now_utc = datetime.now(timezone.utc)
-
-# start_time = datetime.now()
-# print(start_time)
-print(now)
-# print(now_utc)
+print(now_utc)
