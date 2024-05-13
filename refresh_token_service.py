f2d6f728ce25a9b8dbc5ee5e0c31ee847d169c37
2020dataanalysis <2019supersam@gmail.com>
2024-05-13 10:08:18 -0700

    refresh script is working

--- refresh_token_service.py
+++ refresh_token_service.py
@@ -6,7 +6,7 @@ from SchwabAPIClient import SchwabAPIClient
 
 def configure_logging():
     # Set up logging configuration
-    logging.basicConfig(filename='trading_bot.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename='refresh_token.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
     # Define a stream handler to print logs to the console
     console = logging.StreamHandler()
@@ -22,26 +22,25 @@ class TradingBot:
         account_info = self.client.get_account_info()
         self.client.set_account_number_hash_value(account_info[0]['hashValue'])
  
-        if account_info:
-            logger.info("Account information: %s", account_info)
-            logger.info('Client Account Number hash value: %s', self.client.get_account_number_hash_value())
+        # if account_info:
+        #     logger.info("Account information: %s", account_info)
+        #     logger.info('Client Account Number hash value: %s', self.client.get_account_number_hash_value())
 
 
 async def token_timer():
     remaining_time = bot.client.oauth_client.refresh_token_timer()
-    logger.info(f'remaining_time: {remaining_time}')
-    remaining_time = 10
+    # remaining_time = 10
+    logger.info(f'access token expires in: {remaining_time}')
     await asyncio.sleep(remaining_time)
     asyncio.create_task(token_timer())
 
 
 if __name__ == "__main__":
     logger = configure_logging()
-    logger.info('Initial Log')
     credentials_file = 'credentials.json'
     grant_flow_type_filenames_file = 'grant_flow_type_filenames.json'
     bot = TradingBot(credentials_file, grant_flow_type_filenames_file)
-    asyncio.run(token_timer())
-
-    while True:
-        pass
+    
+    loop = asyncio.get_event_loop()
+    loop.create_task(token_timer())
+    loop.run_forever()  # Ensure the event loop keeps running
